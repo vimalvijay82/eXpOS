554ff3ae34b911d38f72b1c5af48fee2cc3a1bf8
Vimal VIjay <76175085+vimalvijay82@users.noreply.github.com>
2023-03-28 23:49:53 +0530

    Add files via upload

---
+++ batch.sh
@@ -0,0 +1,7 @@
+load --os $HOME/Desktop/myexpos/spl/spl_progs/os_st10.xsm
+load --int=7 $HOME/Desktop/myexpos/spl/spl_progs/int7_st10.xsm.xsm
+load --int=10 $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
+load --exhandler $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
+load --int=timer $HOME/Desktop/myexpos/spl/spl_progs/timer_st9.xsm.xsm
+load --init $HOME/Desktop/myexpos/expl/expl_progs/init_st10.xsm
+load --library $HOME/Desktop/myexpos/expl/library.lib
